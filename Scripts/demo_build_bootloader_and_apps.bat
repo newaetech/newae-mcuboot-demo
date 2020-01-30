88567fb3de531c4b67d964951b5c8dbfc3dba69c
Alex Dewar <adewar@newae.com>
2020-01-30 15:47:30 -0400

    update bootloader build and program for bad key

--- Scripts/demo_build_bootloader_and_apps.bat
+++ Scripts/demo_build_bootloader_and_apps.bat
@@ -2,33 +2,35 @@ echo Making bootloader
 cd ..\bootloader
 make PLATFORM=CWLITEARM
 
-pause
+#pause
 
-echo Making AppA
-cd ..\AppA
-make PLATFORM=CWLITEARM
+#echo Making AppA
+#cd ..\AppA
+#make PLATFORM=CWLITEARM
 
-pause
+#pause
 
 echo Making AppB
 cd ..\AppB
+rm APP_B_SIGNED-CWLITEARM.hex
 make PLATFORM=CWLITEARM
-pause
+#pause
 
 echo Signing AppA
 
-cd ..\Scripts
-python .\imgtool.py sign -k root-rsa-2048.pem -S 0x10000 -v 1.0.0 --align 8 -H 0x400 ../AppA/APP_A-CWLITEARM.hex ../AppA/APP_A_SIGNED-CWLITEARM.hex
-pause
+#cd ..\Scripts
+#python .\imgtool.py sign -k root-rsa-2048.pem -S 0x10000 -v 1.0.0 --align 8 -H 0x400 --pad ../AppA/APP_A-CWLITEARM.hex ../AppA/APP_A_SIGNED-CWLITEARM.hex
+#pause
 
 echo Signing AppB
 
 cd ..\Scripts
-python .\imgtool.py sign -k root-rsa-2048.pem -S 0x10000 -v 1.0.0 --align 8 -H 0x400 --pad ../AppB/APP_B-CWLITEARM.hex ../AppB/APP_B_SIGNED-CWLITEARM.hex
-pause 
+python .\imgtool.py sign -k root-rsa-2048-bad.pem -S 0x10000 -v 1.0.0 --align 8 -H 0x400 --pad ../AppB/APP_B-CWLITEARM.hex ../AppB/APP_B_SIGNED-CWLITEARM.hex
+#pause 
 
-echo Programming applications
 cd FirmwareLoader
+
+echo Programming bad applications
 python .\firmware_loader.py
 
 pause
